dc9c68744b2c0038f6944252224e978564da5f47
gibrankasif <61095696+gibrankasif@users.noreply.github.com>
2021-03-25 01:46:15 +0530

    Update Graph.java

--- src/com/gibran/MaximumFlow/Graph.java
+++ src/com/gibran/MaximumFlow/Graph.java
@@ -1,10 +1,46 @@
 package com.gibran.MaximumFlow;
 
-import java.util.List;
-
 public class Graph {
-    private int[][] adjacencyMatrix;
     private int numberOfNodes;
-    private List<Integer>[] adjacencyList;
+    private int[][] adjacencyMatrix;
+
+    public Graph(int numberOfNodes) {
+        this.numberOfNodes = numberOfNodes;
+        adjacencyMatrix = new int[numberOfNodes][numberOfNodes];
+    }
+
+    public int getNumberOfNodes() {
+        return numberOfNodes;
+    }
+
+    public int[][] getAdjacencyMatrix() {
+        return adjacencyMatrix;
+    }
+
+    public void putEdge(int i, int j, int edgeWeight){
+        adjacencyMatrix[i][j] = edgeWeight;
+    }
+
+    public void removeEdge(int i, int j) {
+        adjacencyMatrix[i][j] = 0;
+    }
+
+    public boolean hasEdge(int i, int j){
+        return adjacencyMatrix[i][j] != 0;
+    }
+
+    public int degree(int node) {
+        int numOfEdges = 0;
+        for(int i = 0; i < numberOfNodes; i++) {
+            if(adjacencyMatrix[node][i] != 0){
+                numOfEdges++;
+            }
+        }
+        if(adjacencyMatrix[node][node] != 0){
+            numOfEdges++;
+        }
+        return numOfEdges;
+    }
+
 
 }
